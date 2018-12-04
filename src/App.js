16e782339c16aa5d1cdd3283101c13812b40e5aa
Ndelforno <nicolas.delforno@gmail.com>
2018-12-04 15:08:39 -0500

    nav bar created

--- src/App.js
+++ src/App.js
@@ -2,26 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Homepage from './Homepage';
+import Post from './Post';
+import FavoriteMovies from './FavoriteMovies';
+import FavoriteFood from './FavoriteFood';
+import About from './About';
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>{' '}
+            <Link to="/Canada">Canada</Link>{' '}
+            <Link to="/World">World</Link>{' '}
+            <Link to="/Business">Business</Link>{' '}
+            <Link to="/Tech">Tech</Link>{' '}
+            <Link to="/Politics">Politics</Link>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route path="/Canada" component={Canada} />
+          <Route path="/World" component={World} />
+          <Route path="/Business" component={Business} />
+          <Route path="/Tech" component={Tech} />
+          <Route path="/Politics" component={Politics} />
+        </div>
+      </Router>
+    )
   }
 }
 
