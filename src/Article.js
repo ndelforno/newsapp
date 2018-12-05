a46b05a3bf45951ccb0e35ef144486b177da2f8b
Ndelforno <nicolas.delforno@gmail.com>
2018-12-05 12:51:12 -0500

    loop completed for article

--- src/Article.js
+++ src/Article.js
@@ -1,18 +1,23 @@
 import React, {Component} from 'react';
 
 class Article extends Component {
+
   render () {
-    for (var i = 0; i < this.props.articles.length; i++) {
-      return (
-        <div>
-          <h2>{this.props.articles[i].title}</h2>
-          <img src={`this.props.articles[i].urlToImage`}/>
-          <p>Written by {this.props.articles[i].author}</p>
-          <p>{this.props.articles[i].description}</p>
-          <a href={`this.props.articles[i].url`}>Read the full article</a>
-        </div>
-      )
-    }
+    let articlesList = this.props.articles.map(article =>
+      <div>
+        <h2>{article.title}</h2>
+        <img src={`article.urlToImage`}/>
+        <p>Written by {article.author}</p>
+        <p>{article.description}</p>
+        <a href={`article.url`}>Read the full article</a>
+      </div>)
+
+    return (
+      <div>
+        {articlesList}
+      </div>
+    )
+
   }
 }
 
