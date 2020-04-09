d17752f3bb697841d6df889013689f1cbdabbcc5
Ndelforno <nicolas.delforno@gmail.com>
2020-04-09 11:38:24 -0400

    Added final css

--- src/Article.js
+++ src/Article.js
@@ -5,13 +5,12 @@ class Article extends Component {
   render () {
       let articlesList = this.props.articles.map((article, index) =>
         <a href={article.url}>
-            <div className="article" key={index}>
+            <div className="article grow" key={index}>
                 <img className="articleimg" src={article.urlToImage} onError= "PP.jpeg"/>
                 <h2 className="artTitle">{article.title}</h2>
-                  <p className="artContent">{article.content}</p>
-                  <p className="writtenBy">Written by {article.author}</p>
-                  <p style={{ color: "red" }}>{article.source.name}</p>
-                  <a href={article.url} style={{ color: "#234884" }} >Read the full article</a>
+                <p className="artContent">{article.content}</p>
+
+                <p style={{ color: "red" }}>{article.source.name}</p>
             </div>
         </a>)
 
